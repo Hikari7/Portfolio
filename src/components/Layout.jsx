12033111b14cce1d4989df26e847b2f4f523104c
Hikari7 <h.kobe712@gmail.com>
2022-12-21 13:16:26 -0800

    set mixins and global stylings

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
+//ここでglobal.scssを反映させている
 import "../styles/global.scss";
 
 const Layout = ({ children }) => {
   return (
-    <div className="layout">
+    <div>
       <Navbar />
       <div className="content">{children}</div>
       <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,39 @@
 import { Link } from "gatsby";
 import React from "react";
+import {
+  outline,
+  layout,
+  bg,
+  layout_ul,
+  reset,
+} from "../styles/navbar.module.scss";
+// import {  reset } from "../styles/mixin.scss";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>HK</h1>
-      <div>
-        <Link to="/">HK</Link>
-        <Link to="/about">About</Link>
-        <Link to="/works">Works</Link>
-      </div>
+    <nav className={`${outline} ${layout}`}>
+      <h1>
+        <Link to="/" className={reset}>
+          HK
+        </Link>
+      </h1>
+      <ul className={layout_ul}>
+        <li>
+          <Link to="/" className={reset}>
+            HK
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className={reset}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/works" className={reset}>
+            Works
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
