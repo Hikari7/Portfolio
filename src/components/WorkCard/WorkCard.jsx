3b89cdb51ea1ecf46d5dfaf2b7dbf138a571cbea
Hikari7 <h.kobe712@gmail.com>
2023-02-03 23:22:41 -0800

    added onclick for the work card

--- src/components/WorkCard/WorkCard.jsx
+++ src/components/WorkCard/WorkCard.jsx
@@ -7,9 +7,12 @@ import {
   site,
   cardWrapper,
   hover,
+  desktop,
+  tablet,
 } from "./workcard.module.scss";
 import { DiGithubBadge } from "@react-icons/all-files/di/DiGithubBadge";
 import { FiExternalLink } from "@react-icons/all-files/fi/FiExternalLink";
+import { GrClose } from "@react-icons/all-files/Gr/GrClose";
 import AnimateInView from "../Animation";
 function WorkCard({
   worktitle,
@@ -37,15 +40,49 @@ function WorkCard({
     <>
       <AnimateInView>
         <div
-          className={`${cardWrapper} ${showInfo ? hover : ""}`}
-          onMouseEnter={(e) => showDetails(e)}
-          onMouseLeave={(e) => hideDetails(e)}
+          className={`${cardWrapper} ${showInfo ? hover : ""} ${desktop}`}
           role="presentation"
           id="works"
+          onMouseEnter={(e) => showDetails(e)}
+          onMouseLeave={(e) => hideDetails(e)}
+          // onClick={(e) => showDetails(e)}
+          // onMouseLeave={(e) => hideDetails(e)}
         >
           {showInfo && (
             <div className={overLay}>
               <div className={content}>
+                <h1>{worktitle}</h1>
+                <p>{details}</p>
+                <div className={tools}>
+                  <span>{tool}</span>
+                  <span>{tool2}</span>
+                  <span>{tool3}</span>
+                </div>
+                <div className={site}>
+                  <a href={github}>
+                    <DiGithubBadge size={42} color="#dca0a2" />
+                  </a>
+                  <a href={link}>
+                    <FiExternalLink size={36} color="#dca0a2" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          )}
+          {!showInfo && (
+            <img src={image} className={workImg} alt="workoutLog" />
+          )}
+        </div>
+        <div
+          className={`${cardWrapper} ${showInfo ? hover : ""} `}
+          role="presentation"
+          id="works"
+          onClick={(e) => showDetails(e)}
+        >
+          {showInfo && (
+            <div className={overLay}>
+              <div className={content}>
+                <GrClose />
                 <h1>{worktitle}</h1>
                 <p>{details}</p>
                 <div className={tools}>
