e03da3f6981a479aa4e9db0671f21a1ac0dc2433
Hikari7 <h.kobe712@gmail.com>
2023-03-08 21:49:37 -0800

    modified react icon in the skill component

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -25,8 +25,7 @@ import { SiFirebase } from "@react-icons/all-files/si/SiFirebase";
 import { DiMongodb } from "@react-icons/all-files/di/DiMongodb";
 import { DiGithubBadge } from "@react-icons/all-files/di/DiGithubBadge";
 import { FaFigma } from "@react-icons/all-files/fa/FaFigma";
-// import { AiFillHeart } from "@react-icons/all-files/Ai/AiFillHeart";
-// import { AiFillStar } from "@react-icons/all-files/Ai/AiFillStar";
+import { FaHeart } from "@react-icons/all-files/fa/FaHeart";
 import mui from "../../images/material-ui.svg";
 import styledComponents from "../../images/styled-components.svg";
 import AnimateInView from "../Animation";
@@ -54,11 +53,11 @@ const skills = () => {
                     <SiTypescript size="40px" color="#dca0a2" />
                     <p>
                       <span>
-                        {/* <AiFillStar /> */}
+                        <FaHeart />
                       </span>
                       TypeScript
                       <span>
-                        {/* <AiFillStar /> */}
+                        <FaHeart />
                       </span>
                     </p>
                   </li>
@@ -94,11 +93,11 @@ const skills = () => {
                     <FaReact size="45px" color="#dca0a2" />
                     <p>
                       <span>
-                        {/* <AiFillStar /> */}
+                        <FaHeart />
                       </span>
                       React
                       <span>
-                        {/* <AiFillStar /> */}
+                        <FaHeart />
                       </span>
                     </p>
                   </li>
